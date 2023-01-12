387fcd22b46cc5ea542aec53a68393e0e544b527
nicobesn <besniernicolas1310@gmail.com>
2023-01-12 11:03:48 +0100

    TP 2 SQL (JBF)
    Just Be Food BDD

---
+++ script2.sql
@@ -0,0 +1,52 @@
+-- Exercice 1
+-- Retournez le nom de l'ensemble des produits présents au sein de la base de données.
+
+
+
+-- Exercice 2
+-- Retournez en une requête le nombre de produits présents dans la base de données
+
+
+
+-- Exercice 3
+-- Retournez le nom des produits qui possèdent le critère Végane
+
+
+
+-- Exercice 4
+-- Retournez l'ensemble des produits qui est à base de l'ingrédient pain ciabatta.
+
+
+
+-- Exercice 5
+-- Retournez le nom des produits accompagnés par le nombre d'ingrédients le constituant
+
+
+
+-- Exercice 6
+-- Produisez la requête SQL permettant de retourner le nom du produit ayant le plus d'allergènes ?
+
+
+
+-- Exercice 7
+-- L'administrateur a prévu des critères qui ne sont affectés à aucun produit. Afin de garantir une BDD pertinente, il serait pertinent de les retirer. Veuillez créer la requête SQL permettant de les lister.
+
+
+
+-- Exercice 8
+-- Une équipe de développeurs souhaite ajouter la fonctionnalité permettant de créer un label.
+-- Veuillez leur produire une requête permettant de créer un label nommé 'Label Bleu' et ayant pour url /allergen/labels/label-bleu.png.
+-- Les dates de création et de mise à jour doivent être initialiseés à la date d'execution de la requête.
+
+
+
+-- Exercice 9
+-- Retournez l'ensemble des noms des ingrédients non utilisés au sein des produits ou des tables pivots.
+-- Ordonnés alphabétiquement et sans redondance.
+
+
+
+-- Exercice 10
+-- L'objectif de cette base de données est de pouvoir voir la présence ou l'absence d'allergènes.
+-- Retournez le nom des produits qui ne contiennent pas de lait (milk)
+-- Attention, il est possible d'associer des allergènes aux produits mais aussi aux ingrédients. Votre requête doit le prendre en compte.
